3a791d4b37738030ceb72d0d6eb7ee56a0a80c00
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2024-03-23 18:21:53 +0900

    스택 네비게이션 적용2

---
+++ screens/BangusukTeam.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const BangusukTeam = ({ navigation }) => {
+  return (
+    <View>
+      <Text>BangusukTeam</Text>
+    </View>
+  );
+};
+
+export default BangusukTeam;
---
+++ screens/Tactics.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Tactics = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Tactics</Text>
+    </View>
+  );
+};
+
+export default Tactics;
