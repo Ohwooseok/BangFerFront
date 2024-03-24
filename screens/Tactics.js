d3bf311775e5f88444d37317b60adc9380a6c569
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2024-03-24 17:52:11 +0900

    Tactics 화면 초기 디자인 세팅

--- screens/Tactics.js
+++ screens/Tactics.js
@@ -1,12 +1,64 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, StyleSheet, Image } from "react-native";
+import Back1 from "../assets/Back1.png";
 
 const Tactics = ({ navigation }) => {
   return (
-    <View>
-      <Text>Tactics</Text>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.BackGround}>
+        <Image
+          source={Back1}
+          resizeMode={"stretch"}
+          style={styles.ImageStyle}
+        />
+      </View>
+      <View style={styles.first}></View>
+      <View style={styles.second}>
+        <Text style={styles.Maintext}>BANGUSUK</Text>
+        <Text style={styles.Maintext}>FERGUSON</Text>
+      </View>
+      <View style={styles.third}></View>
+      <View style={styles.four}></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  BackGround: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: -1,
+  },
+  ImageStyle: {
+    width: "100%",
+    height: "100%",
+  },
+  first: {
+    flex: 1,
+  },
+  second: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Maintext: {
+    fontSize: 45,
+    fontWeight: "600",
+    color: "black",
+    textDecorationLine: "underline",
+  },
+  third: {
+    flex: 1,
+  },
+  four: {
+    flex: 1,
+  },
+});
 
 export default Tactics;
